f11e0a36c018ee4bff6c64fd2f05ccc2895e76fd
ex_xiebin <bin2.xie@partner.midea.com>
2016-11-14 10:24:47 +0800

    修改测试

--- src/main/java/com/test/Test1.java
+++ src/main/java/com/test/Test1.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("this is test java program");
+		System.out.println("this is test java program  sadsf");
 	}
 
 }
