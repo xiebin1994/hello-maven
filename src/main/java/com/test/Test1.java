ae5cb5f9f33c78179dc7af186d1eab635095dbf0
ex_xiebin <bin2.xie@partner.midea.com>
2016-11-14 11:07:36 +0800

    f

--- src/main/java/com/test/Test1.java
+++ src/main/java/com/test/Test1.java
@@ -3,7 +3,6 @@ package com.test;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("this is test java program");
-		System.out.println("this is my edit");
 	}
 
 }
